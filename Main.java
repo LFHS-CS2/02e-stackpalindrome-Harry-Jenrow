894e770a2a84cb1df3852ea127f4ab64ef65ab5b
Harry Jenrow <2270266@lfschools.net>
2020-09-18 18:31:57 +0000

    Completed assignment

--- Main.java
+++ Main.java
@@ -22,8 +22,20 @@ class Main {
      */
      static public boolean isPalindrome(String str)
      {
-        
-        return false;
+        Stack<Character> chars = new Stack<Character>();
+        Stack<Character> chars2 = new Stack<Character>();
+        for(int i = 0;i<str.length();i++){
+          chars.push(str.charAt(i));
+        }
+        for(int i = str.length()-1;i>=0;i--){
+          chars2.push(str.charAt(i));
+        }
+        while(!chars.isEmpty()){
+          if(chars.pop()!=chars2.pop()){
+            return false;
+          }
+        }
+        return true;
         
      }
 
